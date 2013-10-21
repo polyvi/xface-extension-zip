daa6fba9c0092e460d79d38797bd3607a71bb3a1
Yangtb <yangtb@polyvi.com>
2013-10-21 15:16:07 +0800

    针对xface修改zip扩展实现workspace下操作文件，引用xfacelib库中的zip库。

--- src/wp/XZipExt.cs
+++ src/wp/XZipExt.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.IO;
-using System.Diagnostics;
-using Windows.Storage;
 using ICSharpCode.SharpZipLib.Zip;
+using xFaceLib.runtime;
+using xFaceLib.Util;
+using xFaceLib.Log;
 
 namespace WPCordovaClassLib.Cordova.Commands
 {
 
-    public class Zip : BaseCommand
+    public class Zip : XBaseCommand
     {
         private enum ErrorCode
         {
@@ -29,15 +30,17 @@ namespace WPCordovaClassLib.Cordova.Commands
             string[] args = JSON.JsonHelper.Deserialize<string[]>(options);
             try
             {
-                string srcFile = args[0];
-                string destZipFile = args[1];
-                if (string.IsNullOrEmpty(srcFile) || destZipFile == null)
+                string workspace = this.app.GetWorkSpace();
+                string srcFile = XUtils.ResolvePath(workspace, args[0]);
+                string destZipFile = XUtils.ResolvePath(workspace, args[1]);
+                if (string.IsNullOrEmpty(srcFile) || string.IsNullOrEmpty(destZipFile)
+                    || srcFile.Equals(workspace))
                 {
                     DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR, ErrorCode.FILE_PATH_ERROR));
                     return;
                 }
-                String abssrcFile = BuildabsPathOnIsolatedStorage(srcFile);
-                String absdestZipFile = BuildabsPathOnIsolatedStorage(destZipFile);
+                String abssrcFile = XUtils.BuildabsPathOnIsolatedStorage(srcFile);
+                String absdestZipFile = XUtils.BuildabsPathOnIsolatedStorage(destZipFile);
                 bool isFileExist = System.IO.File.Exists(abssrcFile);
                 bool isDirExist = Directory.Exists(abssrcFile);
                 if (!isFileExist && !isDirExist)
@@ -45,22 +48,22 @@ namespace WPCordovaClassLib.Cordova.Commands
                     DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR, ErrorCode.FILE_NOT_EXIST));
                     return;
                 }
-                bool flag = ZipLibrary.Zip.ZipFile(abssrcFile, absdestZipFile);
+                bool flag = XUtils.ZipFile(abssrcFile, absdestZipFile);
                 isFileExist = System.IO.File.Exists(absdestZipFile);
                 if (!flag || !isFileExist)
                 {
-                    Debug.WriteLine("zip:" + srcFile + "To path:" + destZipFile + " fail");
+                    XLog.WriteError("zip:" + srcFile + "To path:" + destZipFile + " fail");
                     DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR, ErrorCode.COMPRESS_FILE_ERROR));
                 }
                 else
                 {
-                    Debug.WriteLine("zip:" + srcFile + "To path:" + destZipFile + " success");
+                    XLog.WriteInfo("zip:" + srcFile + "To path:" + destZipFile + " success");
                     DispatchCommandResult(new PluginResult(PluginResult.Status.OK));
                 }
             }
             catch (Exception ex)
             {
-                Debug.WriteLine("zip exception :: " + ex.Message);
+                XLog.WriteError("zip exception :: " + ex.Message);
                 if (ex is ArgumentException || ex is IOException || ex is ZipException)
                 {
                     DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR, ErrorCode.COMPRESS_FILE_ERROR));
@@ -80,18 +83,19 @@ namespace WPCordovaClassLib.Cordova.Commands
                 DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR, ErrorCode.FILE_NOT_EXIST));
                 return;
             }
-
-            string destZipFile = args[1];
-            String absdestZipFile = BuildabsPathOnIsolatedStorage(destZipFile);
+            string workspace = this.app.GetWorkSpace();
+            string destZipFile = XUtils.ResolvePath(workspace, args[1]);
+            string absdestZipFile = XUtils.BuildabsPathOnIsolatedStorage(destZipFile);
             string[] abspaths = new string[paths.Length];
             for (int i = 0; i < paths.Length; i++)
             {
-                if (string.IsNullOrEmpty(paths[i]))
+                abspaths[i] = XUtils.ResolvePath(workspace, paths[i]);
+                if (string.IsNullOrEmpty(abspaths[i]))
                 {
                     DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR, ErrorCode.FILE_PATH_ERROR));
                     return;
                 }
-                abspaths[i] = BuildabsPathOnIsolatedStorage(paths[i]);
+                abspaths[i] = XUtils.BuildabsPathOnIsolatedStorage(abspaths[i]);
                 bool isFileExist = System.IO.File.Exists(abspaths[i]);
                 bool isDirExist = Directory.Exists(abspaths[i]);
                 if (!isFileExist && !isDirExist)
@@ -103,10 +107,10 @@ namespace WPCordovaClassLib.Cordova.Commands
 
             try
             {
-                bool flag = ZipLibrary.Zip.ZipFiles(abspaths, absdestZipFile);
+                bool flag = XUtils.ZipFiles(abspaths, absdestZipFile);
                 if (!flag)
                 {
-                    Debug.WriteLine("zipFiles fail");
+                    XLog.WriteError("zipFiles fail");
                     DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR, ErrorCode.COMPRESS_FILE_ERROR));
                 }
                 else
@@ -116,7 +120,7 @@ namespace WPCordovaClassLib.Cordova.Commands
             }
             catch (Exception ex)
             {
-                Debug.WriteLine("zipFiles exception :: " + ex.Message);
+                XLog.WriteError("zipFiles exception :: " + ex.Message);
                 if (ex is ArgumentException || ex is IOException || ex is ZipException)
                 {
                     DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR, ErrorCode.COMPRESS_FILE_ERROR));
@@ -131,25 +135,26 @@ namespace WPCordovaClassLib.Cordova.Commands
             string[] args = JSON.JsonHelper.Deserialize<string[]>(options);
             try
             {
-                string zipFilePath = args[0];
-                string destPath = args[1];
+                string workspace = this.app.GetWorkSpace();
+                string zipFilePath = XUtils.ResolvePath(workspace, args[0]);
+                string destPath = XUtils.ResolvePath(workspace, args[1]);
                 if (string.IsNullOrEmpty(zipFilePath) || destPath == null)
                 {
                     DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR, ErrorCode.FILE_PATH_ERROR));
                     return;
                 }
-                String abssrcFile = BuildabsPathOnIsolatedStorage(zipFilePath);
-                String absdestPath = BuildabsPathOnIsolatedStorage(destPath);
+                String abssrcFile = XUtils.BuildabsPathOnIsolatedStorage(zipFilePath);
+                String absdestPath = XUtils.BuildabsPathOnIsolatedStorage(destPath);
                 bool isFileExist = System.IO.File.Exists(abssrcFile);
                 if (!isFileExist)
                 {
                     DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR, ErrorCode.FILE_NOT_EXIST));
                     return;
                 }
-                bool flag = ZipLibrary.Zip.unZipFile(abssrcFile, absdestPath);
+                bool flag = XUtils.unZipFile(abssrcFile, absdestPath);
                 if (!flag)
                 {
-                    Debug.WriteLine("unzip:" + zipFilePath + "To path:" + destPath + " fail");
+                    XLog.WriteError("unzip:" + zipFilePath + "To path:" + destPath + " fail");
                     DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR, ErrorCode.UNZIP_FILE_ERROR));
                 }
                 else
@@ -160,7 +165,7 @@ namespace WPCordovaClassLib.Cordova.Commands
             }
             catch (Exception ex)
             {
-                Debug.WriteLine("unzip exception :: " + ex.Message);
+                XLog.WriteError("unzip exception :: " + ex.Message);
                 if (ex is ArgumentException || ex is IOException || ex is ZipException)
                 {
                     DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR, ErrorCode.UNZIP_FILE_ERROR));
@@ -169,22 +174,5 @@ namespace WPCordovaClassLib.Cordova.Commands
                 throw ex;
             }
         }
-
-        /// <summary>
-        /// 将独立存储上文件的相对路径转为绝对路径
-        /// 形如： xFace
-        /// 返回： C:\Data\Users\DefApps\AppData\{productId}\Local\xFace\
-        /// </summary>
-        /// <param name="relativePath">独立存储上文件的相对独立存储的相对路径</param>
-        /// <returns>返回绝对路径</returns>
-        private string BuildabsPathOnIsolatedStorage(string relativePath)
-        {
-            //localFolder fullPath 形如 C:\Data\Users\DefApps\AppData\{productId}\Local\
-            StorageFolder localFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
-            string localFolderPath = localFolder.Path;
-            string fullPath = localFolderPath + "\\" + relativePath;
-            return fullPath;
-        }
-
     }
 }
