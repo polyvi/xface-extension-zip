12d66e99191a89b023de2a6cded320147acbacbf
lird <lird@polyvi.com>
2013-12-18 14:46:35 +0800

    [android]Modify source codes package path

--- src/android/XZipExt.java
+++ src/android/XZipExt.java
@@ -18,7 +18,7 @@
  along with xFace.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package com.polyvi.xface.extension;
+package com.polyvi.xface.extension.zip;
 
 import java.io.File;
 import java.io.FileInputStream;
